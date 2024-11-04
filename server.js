d0dc280c0363285b495697a218fd67309edb3fb7
niranjan03 <32191814+niranjan03@users.noreply.github.com>
2024-11-04 11:22:15 +0530

    update in server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server/index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,12 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']   
-  
-  }));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
